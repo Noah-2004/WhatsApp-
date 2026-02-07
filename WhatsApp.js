3e9b995437c54cc27d1f7dd18b4e178df915e25d
Noah-2004 <jayimnoah@gmail.com>
2026-02-07 18:58:11 +0000

    Add files via upload

---
+++ WhatsApp.js
@@ -0,0 +1,7 @@
+const body =  document.getElementById("body");
+
+if(navigator.online = "false") {
+    alert("can't connect because you are offline");
+} else{
+    body.style.background = "gold";
+}
